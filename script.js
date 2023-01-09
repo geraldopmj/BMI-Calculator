7a72561dc2b8993322a5f4fa61d66d399ee7673d
gerrarrrr <81118137+gerrarrrr@users.noreply.github.com>
2023-01-09 15:13:15 -0300

    1.3 change result color based on bmi

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-document.getElementById("submit").addEventListener("click", bmiCalculator); // listen to click from button submit and activate funcion bmiCalculator
+document.getElementById("submit").addEventListener("click", bmiCalculator); // listen to click from button submit and activate function bmiCalculator
 function bmiCalculator(){
     var height = parseInt(document.getElementById("height").value); //get the height value from field
     var weight = parseFloat(document.getElementById("weight").value); //get the weight value from field
     var bmi; 
     var newHeight = parseFloat(height/100) //parses a value as a string and returns the first number.
     bmi = weight / (newHeight * newHeight); //calculate the BMI
+    
+    if (bmi <= 18.5) {
+        document.getElementById("result").style.color = 'blue';
+    } else if (bmi > 18.5 && bmi <= 24.9) {
+        document.getElementById("result").style.color = 'green';
+    } else if (bmi >= 24.9 && bmi <= 29.9) {
+        document.getElementById("result").style.color = 'yellow';
+    } else if (bmi >= 29.9 && bmi <= 40) {
+        document.getElementById("result").style.color = 'orange';
+    } else {document.getElementById("result").style.color = 'red';}
+
     bmi = bmi.toFixed(1); //method rounds the string to a specified number of decimals specified inside parenthesis.
+        
     document.getElementById("result").innerHTML = "Result: " + bmi; //sets the HTML markup contained within the element(result). 
 }

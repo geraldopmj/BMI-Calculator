99ef1a4ea004cab229f0774193cd52197c37cc80
gerrarrrr <81118137+gerrarrrr@users.noreply.github.com>
2023-01-14 13:30:37 -0300

    1.4 refactored

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-document.getElementById("submit").addEventListener("click", bmiCalculator); // listen to click from button submit and activate function bmiCalculator
+document.getElementById("submit").addEventListener("click", bmiCalculator); // listen to click from button submit and activate function bmiCalculator.
 function bmiCalculator(){
     var height = parseInt(document.getElementById("height").value); //get the height value from field
-    var weight = parseFloat(document.getElementById("weight").value); //get the weight value from field
-    var bmi; 
+    var weight = parseFloat(document.getElementById("weight").value); //get the weight value from field 
     var newHeight = parseFloat(height/100) //parses a value as a string and returns the first number.
-    bmi = weight / (newHeight * newHeight); //calculate the BMI
-    
-    if (bmi <= 18.5) {
-        document.getElementById("result").style.color = 'blue';
-    } else if (bmi > 18.5 && bmi <= 24.9) {
-        document.getElementById("result").style.color = 'green';
-    } else if (bmi >= 24.9 && bmi <= 29.9) {
-        document.getElementById("result").style.color = 'yellow';
-    } else if (bmi >= 29.9 && bmi <= 40) {
-        document.getElementById("result").style.color = 'orange';
-    } else {document.getElementById("result").style.color = 'red';}
-
+    var bmi = weight / (newHeight * newHeight); //calculate the BMI
     bmi = bmi.toFixed(1); //method rounds the string to a specified number of decimals specified inside parenthesis.
+    
+    //Condition to change the result color based on values!
+    if (bmi <= 18.5) {document.getElementById("result").style.color = 'blue';
+    } else if (bmi > 18.5 && bmi <= 24.9) {document.getElementById("result").style.color = 'green';
+    } else if (bmi >= 24.9 && bmi <= 29.9) {document.getElementById("result").style.color = 'yellow';
+    } else if (bmi >= 29.9 && bmi <= 40) {document.getElementById("result").style.color = 'orange';
+    } else    {document.getElementById("result").style.color = 'red';}
         
-    document.getElementById("result").innerHTML = "Result: " + bmi; //sets the HTML markup contained within the element(result). 
+    document.getElementById("result").innerHTML = "BMI: " + bmi; //sets the HTML markup contained within the element(result). 
 }
